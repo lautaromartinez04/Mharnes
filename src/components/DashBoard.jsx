bcf20e8f1008f68c35b463efa748e7230e8469c9
lautaromartinez04 <lautarom403@gmail.com>
2024-10-20 20:08:40 -0300

    dahsboard completo

---
+++ src/components/DashBoard.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import '../assets/css/dashboard.css'
+
+export const DashBoard = () => {
+  return (
+    <div class="row row-cols-1 row-cols-md-4 g-4 my-5 w-100 text-center">
+        <div class="col">
+            <div class="card h-100 mx-2">
+            <div class="card-body">
+                <h5 class="card-title">Energia Generada</h5>
+                <p class="card-text valor">1000<span class="unidad">kWh</span></p>
+                <p class="card-text">Anualmente a través de nuestros paneles solares.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100 mx-2" >
+            <div class="card-body">
+                <h5 class="card-title">Arboles Plantados</h5>
+                <p class="card-text valor">56<span class="unidad"></span></p>
+                <p class="card-text">Contribuyendo a la reforestación y mejora del medio ambiente.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100 mx-2">
+            <div class="card-body">
+                <h5 class="card-title">Visitantes</h5>
+                <p class="card-text valor">198<span class="unidad">kWh</span></p>
+                <p class="card-text">desde el lanzamiento del programa <NavLink to="/DelAulaAlTambo">Del Aula Al Tambo</NavLink></p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100 mx-2">
+            <div class="card-body">
+                <h5 class="card-title">Produccion</h5>
+                <p class="card-text valor">40M<span class="unidad">lts</span></p>
+                <p class="card-text">De leche de la mas alta calidad.</p>
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/routes/Inicio.jsx
+++ src/routes/Inicio.jsx
@@ -1,33 +1,66 @@
-import React from 'react'
-import "../assets/css/inicio.css"
-import tambo from "../assets/images/tambo.webp"
-import vacas from "../assets/images/vacas.webp"
-import ternero from "../assets/images/ternero.webp"
+import React from "react";
+import "../assets/css/inicio.css";
+import tambo from "../assets/images/tambo.webp";
+import vacas from "../assets/images/vacas.webp";
+import ternero from "../assets/images/ternero.webp";
+import { DashBoard } from "../components/DashBoard";
 
 export const Inicio = () => {
   return (
     <>
-      <div id="carouselExampleAutoplaying" class="carousel slide inicio" data-bs-ride="carousel">
+      <div className="text-center my-5 mx-5">
+        <h2 >
+          Bienvenidos a Grupo Mharnes, donde la innovación, el bienestar animal
+          y la sostenibilidad se unen para producir la mejor leche, con el
+          compromiso de un futuro más saludable y responsable
+        </h2>
+      </div>
+      <div
+        id="carouselExampleAutoplaying"
+        class="carousel slide inicio"
+        data-bs-ride="carousel"
+      >
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src={tambo} class="d-block w-100" alt="..."/>
+            <img src={tambo} class="d-block w-100" alt="..." />
           </div>
           <div class="carousel-item">
-            <img src={vacas} class="d-block w-100" alt="..."/>
+            <img src={vacas} class="d-block w-100" alt="..." />
           </div>
           <div class="carousel-item">
-            <img  src={ternero} class="d-block w-100" alt="..."/>
+            <img src={ternero} class="d-block w-100" alt="..." />
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="prev"
+        >
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="next"
+        >
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
       </div>
+
+      <div className="my-5 mx-5 text-center ">
+        <p>
+          En Grupo Mharnes, fusionamos tecnología de vanguardia con un profundo compromiso por el bienestar animal para 
+          asegurar la más alta calidad en la producción de leche. Cada gota es obtenida y procesada con esmero, reflejándose 
+          en los productos lácteos de Duy Amis, llevando así la excelencia de nuestro tambo a las mesas de los consumidores. 
+          Juntos, compartimos una visión sostenible y un legado familiar que supera los 30 años de tradición
+        </p>
+      </div>
+
+      <DashBoard />
     </>
-  )
-}
+  );
+};
