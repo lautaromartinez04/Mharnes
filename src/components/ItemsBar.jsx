ccea7ce12b04a62b6192f487da7f6dc1f273eeb4
lautaromartinez04 <lautarom403@gmail.com>
2024-10-21 11:01:21 -0300

    .

--- src/components/ItemsBar.jsx
+++ src/components/ItemsBar.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { NavLink, useLocation } from 'react-router-dom';
 import "../assets/css/ItemsBar.css";
 
 export const ItemsBar = () => {
   const { hash } = useLocation();
+  const collapseButtonRef = useRef(null); // Referencia al botón de colapso
 
   useEffect(() => {
     if (hash) {
@@ -60,57 +61,63 @@ export const ItemsBar = () => {
     e.currentTarget.querySelector('.dropdown-menu').classList.remove('show');
   };
 
+  const handleNavLinkClick = () => {
+    if (collapseButtonRef.current && window.getComputedStyle(collapseButtonRef.current).display !== 'none') {
+      collapseButtonRef.current.click(); // Simula un clic para colapsar el menú
+    }
+  };
+
   return (
     <>
       <nav style={navbarStyle} className="navbar navbar-expand-lg navbar-dark">
         <div className="container-fluid opciones">
-          <button className="navbar-toggler mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" >
+          <button ref={collapseButtonRef} className="navbar-toggler mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <NavLink to="/Inicio" onClick={scrollToTop} className="nav-link navboton">
+                <NavLink to="/Inicio" onClick={() => { scrollToTop(); handleNavLinkClick(); }} className="nav-link navboton">
                   Inicio
                 </NavLink>
               </li>
 
-              <li className="nav-item dropdown" onClick={scrollToTop} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                <NavLink to="/SobreNosotros" className="nav-link navboton">
+              <li className="nav-item dropdown" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <NavLink to="/SobreNosotros" onClick={scrollToTop} className="nav-link navboton">
                   Sobre Nosotros
                 </NavLink>
                 <ul className="dropdown-menu">
-                  <li><NavLink to="/SobreNosotros/#historia" className="dropdown-item">Historia</NavLink></li>
-                  <li><NavLink to="/SobreNosotros/#mision" className="dropdown-item">Misión</NavLink></li>
-                  <li><NavLink to="/SobreNosotros/#vision" className="dropdown-item">Visión</NavLink></li>
-                  <li><NavLink to="/SobreNosotros/#valores" className="dropdown-item">Valores</NavLink></li>
-                  <li><NavLink to="/SobreNosotros/#innovacion" className="dropdown-item">Innovación</NavLink></li>
-                  <li><NavLink to="/SobreNosotros/#calidad" className="dropdown-item">Calidad</NavLink></li>
+                  <li><NavLink to="/SobreNosotros/#historia" onClick={handleNavLinkClick} className="dropdown-item">Historia</NavLink></li>
+                  <li><NavLink to="/SobreNosotros/#mision" onClick={handleNavLinkClick} className="dropdown-item">Misión</NavLink></li>
+                  <li><NavLink to="/SobreNosotros/#vision" onClick={handleNavLinkClick} className="dropdown-item">Visión</NavLink></li>
+                  <li><NavLink to="/SobreNosotros/#valores" onClick={handleNavLinkClick} className="dropdown-item">Valores</NavLink></li>
+                  <li><NavLink to="/SobreNosotros/#innovacion" onClick={handleNavLinkClick} className="dropdown-item">Innovación</NavLink></li>
+                  <li><NavLink to="/SobreNosotros/#calidad" onClick={handleNavLinkClick} className="dropdown-item">Calidad</NavLink></li>
                 </ul>
               </li>
 
-              <li className="nav-item dropdown" onClick={scrollToTop} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                <NavLink to="/NuestrasPracticas" className="nav-link navboton">
+              <li className="nav-item dropdown" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <NavLink to="/NuestrasPracticas" onClick={scrollToTop} className="nav-link navboton">
                   Nuestras prácticas
                 </NavLink>
                 <ul className="dropdown-menu">
-                  <li><NavLink to="/NuestrasPracticas/#bienestar" className="dropdown-item">Bienestar animal</NavLink></li>
-                  <li><NavLink to="/NuestrasPracticas/#sostenibilidad" className="dropdown-item">Sostenibilidad ambiental</NavLink></li>
-                  <li><NavLink to="/NuestrasPracticas/#tecnologia" className="dropdown-item">Tecnología avanzada</NavLink></li>
-                  <li><NavLink to="/NuestrasPracticas/#compromiso" className="dropdown-item">Compromiso social</NavLink></li>
+                  <li><NavLink to="/NuestrasPracticas/#bienestar" onClick={handleNavLinkClick} className="dropdown-item">Bienestar animal</NavLink></li>
+                  <li><NavLink to="/NuestrasPracticas/#sostenibilidad" onClick={handleNavLinkClick} className="dropdown-item">Sostenibilidad ambiental</NavLink></li>
+                  <li><NavLink to="/NuestrasPracticas/#tecnologia" onClick={handleNavLinkClick} className="dropdown-item">Tecnología avanzada</NavLink></li>
+                  <li><NavLink to="/NuestrasPracticas/#compromiso" onClick={handleNavLinkClick} className="dropdown-item">Compromiso social</NavLink></li>
                 </ul>
               </li>
 
-              <li className="nav-item dropdown" onClick={scrollToTop} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                <NavLink to="/DelAulaAlTambo" className="nav-link navboton">
+              <li className="nav-item dropdown" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <NavLink to="/DelAulaAlTambo" onClick={scrollToTop} className="nav-link navboton">
                   Del aula al tambo
                 </NavLink>
                 <ul className="dropdown-menu">
-                  <li><NavLink to="/DelAulaAlTambo/#educacion" className="dropdown-item">Educación y Conciencia</NavLink></li>
-                  <li><NavLink to="/DelAulaAlTambo/#experiencia" className="dropdown-item">Experiencia Práctica</NavLink></li>
-                  <li><NavLink to="/DelAulaAlTambo/#tecnologia" className="dropdown-item">Tecnología e Innovación</NavLink></li>
-                  <li><NavLink to="/DelAulaAlTambo/#bienestar" className="dropdown-item">Bienestar Animal</NavLink></li>
-                  <li><NavLink to="/DelAulaAlTambo/#sostenibilidad" className="dropdown-item">Sostenibilidad</NavLink></li>
+                  <li><NavLink to="/DelAulaAlTambo/#educacion" onClick={handleNavLinkClick} className="dropdown-item">Educación y Conciencia</NavLink></li>
+                  <li><NavLink to="/DelAulaAlTambo/#experiencia" onClick={handleNavLinkClick} className="dropdown-item">Experiencia Práctica</NavLink></li>
+                  <li><NavLink to="/DelAulaAlTambo/#tecnologia" onClick={handleNavLinkClick} className="dropdown-item">Tecnología e Innovación</NavLink></li>
+                  <li><NavLink to="/DelAulaAlTambo/#bienestar" onClick={handleNavLinkClick} className="dropdown-item">Bienestar Animal</NavLink></li>
+                  <li><NavLink to="/DelAulaAlTambo/#sostenibilidad" onClick={handleNavLinkClick} className="dropdown-item">Sostenibilidad</NavLink></li>
                 </ul>
               </li>
             </ul>
